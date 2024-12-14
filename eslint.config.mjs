2f55f604e32cfc6ef25b947b048ce2f4f72c2458
RotcehRuiz <ruiz.hectoralum023@gmail.com>
2024-12-14 01:29:30 -0600

    Test Husky with lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,32 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import globals from "globals";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react: pluginReact, // Configuración correcta de `plugins` como objeto
+    },
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "single"],
+      "no-unused-vars": "error",
+    },
+    settings: {
+      react: {
+        version: "detect", // Detecta automáticamente la versión desde package.json
+      },
+    },
+  },
 ];
